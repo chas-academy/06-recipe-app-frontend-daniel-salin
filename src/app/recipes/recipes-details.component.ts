94172091f88df72b0326b1aba51355b043a150d7
daniel-salin <daniel.salin@chasacademy.se>
2019-03-25 10:38:59 +0100

    :bug: Fix bug where users can add multiple instances of the same recipe to the same list. - Fixes #1

--- src/app/recipes/recipes-details.component.ts
+++ src/app/recipes/recipes-details.component.ts
@@ -17,6 +17,7 @@ export class RecipesDetailsComponent implements OnInit {
   lists: Lists[];
   observable: string;
   loggedIn: boolean;
+  recipeExists;
 
   constructor(
     private route: ActivatedRoute,
@@ -36,7 +37,16 @@ export class RecipesDetailsComponent implements OnInit {
         LIST.push(new Lists(data.attributes.title, parseInt(localStorage.getItem('uid')) , data.attributes.recipes, data.id));
         return LIST;
       });
-      return this.lists = LIST;
+
+      this.recipeExists = [];
+      LIST.map(list => {
+        (list.recipes === null || list.recipes.length === 0)
+        ? this.recipeExists.push({title: list.title, id: list.id, match:false})
+        : (list.recipes.some(element => element.id === this.recipeId.id)) 
+          ? this.recipeExists.push({title: list.title, id: list.id, match:true})
+          : this.recipeExists.push({title: list.title, id: list.id, match:false})
+      })
+     return this.lists = LIST;
     });
 
     const RECIPE = [];
@@ -62,13 +72,32 @@ export class RecipesDetailsComponent implements OnInit {
           calories,
           display
         )
-      );
+        );
       return RECIPE;
     });
     this.recipe = RECIPE;
   }
 
+  renderListIcon(list) {
+    const matchingList = this.recipeExists.filter(element => element.id === list.id);
+    return matchingList[0].match;
+  }
+
+removeRecipe(list) {
+  this.listService.deleteRecipe(list, this.recipeId['id']).subscribe();
+  (this.recipeExists.map(element => {
+    if (element.id === list.id) {
+      element.match = false
+    }
+  })); 
+}
+
   saveRecipe(list) {
     this.listService.addRecipe(this.recipe, list).subscribe();
+    (this.recipeExists.map(element => {
+      if (element.id === list.id) {
+        element.match = true
+      }
+    })); 
   }
 }
